634f37ba68845510e64aaf6ac31eb981b3bc9229
Jefferson Silva <jeffsilva01.dev@gmail.com>
2022-04-06 23:28:16 -0300

    remove search box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 import type { NextPage } from 'next';
-import { SearchBox } from '../components/SearchBox';
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <SearchBox />
-      <h1>hello world!</h1>
-    </>
-  );
+  return <h1>hello world!</h1>;
 };
 
 export default Home;
