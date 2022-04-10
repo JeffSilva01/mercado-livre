4625af1d54ebc825cfa477874182681e5596f7d2
Jefferson Silva <jeffsilva01.dev@gmail.com>
2022-04-10 19:40:52 -0300

    baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://mercado-livre-ngk73w5em-jeffsilva01.vercel.app',
+  baseURL: 'https://mercado-livre-mgsla2gao-jeffsilva01.vercel.app',
 });
 
 export default api;
