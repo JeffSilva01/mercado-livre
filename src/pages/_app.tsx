2e4cd65fec44efb614bf06b219ead299c177d7a2
Jefferson Silva <jeffsilva01.dev@gmail.com>
2022-04-17 12:34:11 -0300

    remove div

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <SearchBox />
-      <div className='container'>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </>
   );
 }
